d8935830ee6daad1abd9d6bf60afcff85ee53d32
bpastorelli <brunopastorelli@hotmail.com>
2025-08-29 17:09:22 -0300

    Ajuste

--- src/main/java/br/com/visita/security/config/WebSecurity.java
+++ src/main/java/br/com/visita/security/config/WebSecurity.java
@@ -31,7 +31,9 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(requests -> requests
-                //.mvcMatchers(HttpMethod.GET, "https://j77akndu44.execute-api.sa-east-1.amazonaws.com/prod/**").permitAll()
+                .mvcMatchers(HttpMethod.GET, "https://j77akndu44.execute-api.sa-east-1.amazonaws.com/prod/**").permitAll()
+                .mvcMatchers(HttpMethod.POST, "https://j77akndu44.execute-api.sa-east-1.amazonaws.com/prod/**").permitAll()
+                .mvcMatchers(HttpMethod.PUT, "https://j77akndu44.execute-api.sa-east-1.amazonaws.com/prod/**").permitAll()
                 .mvcMatchers(HttpMethod.GET, "**/sgc/visitante/**").permitAll()
                 .mvcMatchers(HttpMethod.POST, "**/sgc/visitante/**").permitAll()
                 .mvcMatchers(HttpMethod.PUT, "**/sgc/visitante/**").permitAll()
